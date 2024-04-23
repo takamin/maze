474f9e2cc0a95c50c949e7b8e99e89aaa6d91443
Koji Takami <vzg03566@gmail.com>
2024-04-23 12:36:43 +0900

    :bug: 棒倒し法を正しく実装し、立ち入りできない孤立した通路が生成されないようした

--- src/maze.mjs
+++ src/maze.mjs
@@ -111,6 +111,14 @@ export class Maze {
    * ランダムに迷路を作成する
    */
   createRandomMaze() {
+    const isWallAt = (x, y) => {
+      const xx = x + 1;
+      const yy = y + 1;
+      if (xx < 0 || xx >= this.sizeX || yy < 0 || yy >= this.sizeY) {
+        return true;
+      }
+      return this.map[yy][xx] === Maze.Wall;
+    };
     const putWall = (x, y) => {
       this.map[y + 1][x + 1] = Maze.Wall;
     };
@@ -119,15 +127,23 @@ export class Maze {
     for (let y = 1; y < innerSizeY; y += 2) {
       for (let x = 1; x < innerSizeX; x += 2) {
         putWall(x, y);
-        const direction = Math.floor(Math.random() * 4);
-        switch (direction) {
-          case 0: putWall(x, y - 1); break;
-          case 1: putWall(x + 1, y); break;
-          case 2: putWall(x, y + 1); break;
-          case 3: putWall(x - 1, y); break;
-          default:
-            break;
+        const direction = {
+          top: { x: x, y: y - 1 },
+          left: { x: x - 1, y: y },
+          right: { x: x + 1, y: y },
+          bottom: { x: x, y: y + 1 },
         }
+        if (y > 1 || isWallAt(x, y - 1)) {
+          delete direction.top;
+        }
+        if (x > 1 || isWallAt(x - 1, y)) {
+          delete direction.left;
+        }
+        const dirCount = Object.keys(direction).length;
+        const dirCoord = Object.values(direction);
+        const dirIndex = Math.floor(Math.random() * dirCount);
+        const { x: wallX, y: wallY } = dirCoord[dirIndex];
+        putWall(wallX, wallY);
       }
     }
   }
