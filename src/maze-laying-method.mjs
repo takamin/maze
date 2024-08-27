3d08ffc9cc8678e1e42ffccfa5e62723943f746b
Koji Takami <vzg03566@gmail.com>
2024-08-27 14:33:55 +0900

    :construction: 棒倒し法でランダムに壁を描くように変更中

--- src/maze-laying-method.mjs
+++ src/maze-laying-method.mjs
@@ -3,24 +3,54 @@ import { Maze } from "./maze.mjs";
 export class MazeLayingMethod extends Maze {
   stickMapX = null;
   stickMapY = null;
+  stickMap = null;
   stickMapLength = null;
-  stickIndex = null;
   init() {
     this.stickMapX = (this.sizeX - 3) / 2;
     this.stickMapY = (this.sizeY - 3) / 2;
     this.stickMapLength = this.stickMapX * this.stickMapY;
-    this.stickIndex = 0;
+    this.stickMap = Array(this.stickMapLength).fill(null);
+  }
+  /**
+   * 壁の起点となる支柱を立てる竪穴の数を返す。
+   * すでに建てられている支柱の数は含まない。
+   * @returns {number} 未設定の支柱の数
+   */
+  getStandingSticksCount() {
+    const count = this.stickMap.filter((stick) => stick == null).length;
+    return count;
+  }
+  /**
+   * 壁を伸ばす始点をランダムに決定する
+   * @param {number} standingSticks 秋状態の支柱の数
+   * @returns {number}
+   */
+  getNextEmptyStickIndex(standingSticks) {
+    const standingStickIndex = Math.floor(Math.random() * standingSticks);
+    let nullCount = 0;
+    for (let i = 0; i < this.stickMapLength; i++) {
+      if (this.stickMap[i] == null) {
+        if (nullCount === standingStickIndex) {
+          return i;
+        }
+        nullCount++;
+      }
+    }
+    return -1;
   }
   /**
    * 棒倒し法で迷路を作成する
    */
   generate() {
-    if(this.stickIndex >= this.stickMapLength) {
+    // 倒していない棒の数
+    const standingSticks = this.getStandingSticksCount();
+    if (standingSticks === 0) {
       return true; // 終了
     }
-    const sx = this.stickIndex % this.stickMapX;
-    const sy = Math.floor(this.stickIndex / this.stickMapX);
-    this.stickIndex++;
+    const stickIndex = this.getNextEmptyStickIndex(standingSticks);
+    this.stickMap[stickIndex] = true;
+    const sx = stickIndex % this.stickMapX;
+    const sy = Math.floor(stickIndex / this.stickMapX);
     const x = sx * 2 + 2;
     const y = sy * 2 + 2;
     this.setWallAt(x, y);
@@ -30,13 +60,22 @@ export class MazeLayingMethod extends Maze {
       right: { x: x + 1, y: y },
       bottom: { x: x, y: y + 1 },
     }
-    if (y > 2 || this.isWall(x, y - 1)) {
+    if (y > 2 && this.isWall(x, y - 1)) {
       delete direction.top;
     }
-    if (x > 2 || this.isWall(x - 1, y)) {
+    if (x > 2 && this.isWall(x - 1, y)) {
       delete direction.left;
     }
+    if (x < this.sizeX - 3 && this.isWall(x + 1, y)) {
+      delete direction.right;
+    }
+    if (y < this.sizeY - 3 && this.isWall(x, y + 1)) {
+      delete direction.bottom;
+    }
     const dirCount = Object.keys(direction).length;
+    if(dirCount === 0) {
+      return [{ x, y }];
+    }
     const dirCoord = Object.values(direction);
     const dirIndex = Math.floor(Math.random() * dirCount);
     const { x: wallX, y: wallY } = dirCoord[dirIndex];
